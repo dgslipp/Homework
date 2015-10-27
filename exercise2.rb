41b87498a5c94e8fc26115cd20016f6a94ae608e
dgslipp <davidslipp@Davids-MacBook-Pro.local>
2015-10-27 16:23:36 -0400

    lesson two

---
+++ exercise2.rb
@@ -0,0 +1,20 @@
+puts "Problem One"
+Dinner_Cost = 55.00
+Tip_Percent = 0.20
+Tip = Dinner_Cost * Tip_Percent
+puts "Dinner Tip = $#{Tip}"
+
+puts "Problem Two"
+puts 4.to_s
+
+
+puts "Problem Three"
+n = 45628
+x = 7839
+y = n * x
+puts "The multiplication of n x x = #{y}"
+
+
+puts "Probelm Four"
+puts (true && false) || (false && true) || (false && false)
+
