1a4665bf5e5fe0f691ab76c2eb2d0d5db8bc7313
dgslipp <davidslipp@Davids-MacBook-Pro.local>
2015-10-28 06:53:31 -0400

    print numbers 1 thru 100

---
+++ exercise4.rb
@@ -0,0 +1,10 @@
+# class Multiples
+	# def multiples
+	one_to_one_hundred = (1..100)
+	# multiples = array.new
+	# for i in one_to_one_hundred	
+	one_to_one_hundred.each do |num|
+	print num.to_s + " "
+end
+# end
+# end
