9336e75fba17498fc52a9355feed368efaae72b4
dgslipp <davidslipp@Davids-MacBook-Pro.local>
2015-10-27 23:57:22 -0400

    ask your age to print birth year

--- exercise3.rb
+++ exercise3.rb
@@ -1,3 +1,8 @@
 puts "what is your name"
 name = gets.chomp
 puts "Hi #{name}!"
+puts "how old are you?"
+age = gets.chomp
+time = Time.new
+y = time.year
+puts "you were born in #{y.to_i - age.to_i}"
